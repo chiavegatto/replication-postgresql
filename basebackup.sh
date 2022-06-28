a539472781a59736fe397922c3beadc2445ccabf
Rafael Chiavegatto <rafael.chiavegatto@fpf.br>
2022-06-28 17:39:24 -0400

    Alterando a parte do cluster.

--- basebackup.sh
+++ basebackup.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 
+/etc/init.d/postgresql stop
+echo "iniciando a exclusão dos arquivos"
 rm -rf /var/lib/postgresql/data/*
+echo "Finalizando a exclusão dos arquivos"
+
+echo "iniciando o backup"
 pg_basebackup -R -h master -U replicador -D /var/lib/postgresql/data -P
-/usr/local/bin/docker-entrypoint.sh postgres 
+echo "Finalizado o backup"
